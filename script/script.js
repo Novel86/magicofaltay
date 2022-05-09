e6a2e2d3616cf5e20c68e8aa7bf56e7792dc9868
Roman Krets <romankrets@gmail.com>
2022-05-09 23:12:29 +0700

    befor-adaptive

---
+++ script/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+	$('.first__utp-slider').slick({
+		dots: true,
+		speed: 1300,
+		easing: 'ease-in-out',
+		autoplay: true,
+		autoplayspeed: 2000,
+		pauseonfocus: false,
+		draggable: false,
+	});
+});
+
+$(document).ready(function () {
+	$('.galary-slider').slick({
+		dots: true,
+		speed: 1300,
+		easing: 'ease-in-out',
+		autoplay: true,
+		autoplayspeed: 2000,
+		pauseonfocus: false,
+		draggable: false,
+	});
+});
