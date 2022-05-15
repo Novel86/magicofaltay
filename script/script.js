4498a9575699159226cf998f723283707d70d0d3
Roman Krets <romankrets@gmail.com>
2022-05-16 01:22:29 +0700

    свайпер в галерее

--- script/script.js
+++ script/script.js
@@ -21,3 +21,65 @@ $(document).ready(function () {
 		draggable: false,
 	});
 });
+
+new Swiper('.gallary-slider', {
+	slidesPerView: 2.2,
+	spaceBetween: 30,
+	// Responsive breakpoints
+	breakpoints: {
+		// when window width is >= 320px
+		320: {
+			slidesPerView: 1.2,
+			spaceBetween: 20
+		},
+		// when window width is >= 480px
+		// 480: {
+		// 	slidesPerView: 3,
+		// 	spaceBetween: 30
+		// },
+		// when window width is >= 640px
+		767: {
+			slidesPerView: 2.2,
+			spaceBetween: 30
+		}
+	},
+	autoplay: {
+		delay: 0,
+		disableOnInteraction: false,
+		pauseOnMouseEnter: true
+	},
+	speed: 6000,
+	loop: true,
+
+});
+
+new Swiper('.gallary-revers', {
+	slidesPerView: 2.2,
+	spaceBetween: 30,
+	// Responsive breakpoints
+	breakpoints: {
+		// when window width is >= 320px
+		320: {
+			slidesPerView: 1.2,
+			spaceBetween: 20
+		},
+		// when window width is >= 480px
+		// 480: {
+		// 	slidesPerView: 3,
+		// 	spaceBetween: 30
+		// },
+		// when window width is >= 640px
+		767: {
+			slidesPerView: 2.2,
+			spaceBetween: 30
+		}
+	},
+	autoplay: {
+		delay: 0,
+		disableOnInteraction: false,
+		pauseOnMouseEnter: true,
+		// reverseDirection: true
+	},
+	speed: 6000,
+	loop: true
+})
