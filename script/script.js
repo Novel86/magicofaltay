f21915d69574c39ecabaa3d8b92ebba5845a9a07
Roman Krets <romankrets@gmail.com>
2022-06-11 23:51:09 +0700

    form is working

--- script/script.js
+++ script/script.js
@@ -1,4 +1,79 @@
 "use strict"
+
+document.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('form');
+	form.addEventListener('submit', formSend);
+
+	async function formSend(e) {
+		e.preventDefault();
+
+		let error = formValidate(form);
+
+		let formData = new FormData(form);
+		// 		// formData.append('image', formImage.files[0]);
+
+		if (error === 0) {
+			form.classList.add('_sending');
+			let response = await fetch('sendmail.php', {
+				method: 'POST',
+				body: formData
+			});
+			if (response.ok) {
+				let result = await response.json();
+				alert(result.message);
+				form.reset();
+				form.classList.remove('_sending');
+			} else {
+				alert("Ошибка");
+				form.classList.remove('_sending');
+			}
+		} else {
+			alert('Заполните обязательные поля');
+		}
+
+		// 	}
+
+
+		function formValidate(form) {
+			let error = 0;
+			let formReq = document.querySelectorAll('._req');
+
+			for (let index = 0; index < formReq.length; index++) {
+				const input = formReq[index];
+				formRemoveError(input);
+
+				if (input.classList.contains('_email')) {
+					if (emailTest(input)) {
+						formAddError(input);
+						error++;
+					}
+				} else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+					formAddError(input);
+					error++;
+				} else {
+					if (input.value === '') {
+						formAddError(input);
+						error++;
+					}
+				}
+			}
+			return error;
+		}
+		function formAddError(input) {
+			input.parentElement.classList.add('_error');
+			input.classList.add('_error');
+		}
+		function formRemoveError(input) {
+			input.parentElement.classList.remove('_error');
+			input.classList.remove('_error');
+		}
+		//Функция теста email
+		function emailTest(input) {
+			return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+		}
+	}
+});
+
 // Прокрутка при клике
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
 if (menuLinks.length > 0) {
@@ -36,22 +111,10 @@ if (iconMenu) {
 		iconMenu.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
 	});
-}
+};
 
 $(document).ready(function () {
 	$('.first__utp-slider').slick({
-		dots: true,
-		speed: 1300,
-		easing: 'ease-in-out',
-		autoplay: true,
-		autoplayspeed: 2000,
-		pauseonfocus: false,
-		draggable: false,
-	});
-});
-
-$(document).ready(function () {
-	$('.galary-slider').slick({
 		dots: true,
 		speed: 1300,
 		easing: 'ease-in-out',
@@ -140,7 +203,6 @@ new Swiper('.gallary-revers', {
 })
 
 // слайдер аккордеон день#
-
 $(document).ready(function () {
 	$('.day__title').click(function (event) {
 		// Аккордион
---
+++ sendmail.php
@@ -0,0 +1,50 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('novel86@mail.ru', 'Magic of Altay');
+	//Кому отправить
+	$mail->addAddress('romankrets@gmail.com');
+	//Тема письма
+	$mail->Subject = 'Заявка на тур с сайта';
+
+
+	//Тело письма
+	$body = 'Встречайте супер письмо!     ';
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='Имя: '.$_POST['name'].'   ';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='E-mail: '.$_POST['email'].'   ';
+	}
+	if(trim(!empty($_POST['phone']))){
+		$body.='Телефон: '.$_POST['phone'].'   ';
+	}
+	if(trim(!empty($_POST['message']))){
+		$body.='Сообщение: '.$_POST['message'].'   ';
+	}
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
